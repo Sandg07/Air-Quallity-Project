524f30879da79f177ebd79e914eec664d74b8888
Paul Lallemand <paul.lallemand92@gmail.com>
2021-12-10 16:12:55 +0100

    api Controller change again

--- air-quality/app/Http/Controllers/ApiController.php
+++ air-quality/app/Http/Controllers/ApiController.php
@@ -56,7 +56,7 @@ class ApiController extends Controller
 
         foreach ($transformed as $key => $finalObject) {
             $finalObject->value = $data[$key]->value;
-            $finalObject->value = $data[$key]->index;
+            $finalObject->index = $data[$key]->index;
         }
 
         dd($transformed);
