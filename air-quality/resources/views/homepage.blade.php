4a332ae3b04159fb804991933e5c1bf1bfa34f58
Sandrine V <sandrine.gardini@gmail.com>
2021-12-13 12:28:01 +0100

    first commit homepage view

--- air-quality/resources/views/homepage.blade.php
+++ air-quality/resources/views/homepage.blade.php
@@ -2,8 +2,34 @@
 
 @section('title', 'Homepage Air Quality')
 
+@section('css')
+    {{-- link for the map --}}
+    <link href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" rel="stylesheet" />
+@endsection
+
 
 @section('content')
 
-    This is my homepage !
+    <header>
+        <h1>Air Quality app</h1>
+        <h3>Some text about app</h3>
+    </header>
+
+    <main>
+        <div>
+            <h2>Section 2 Title</h2>
+            <div>@include('searchbox')
+            </div>
+        </div>
+
+
+        <div>
+            <h2>Section 3 Features</h2>
+            <div>
+                features
+            </div>
+        </div>
+    </main>
+
+
 @endsection
--- air-quality/resources/views/searchbox.blade.php
+++ air-quality/resources/views/searchbox.blade.php
@@ -1,28 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" rel="stylesheet"/>
-    <title>Document</title>
-</head>
-<body>
-    
-    <h2>Search</h2>
+<h2>Search</h2>
 
-    <div class="searchDiv">
-        <form action="" method="get">
-            <input type="search" name="searchboxMap" id="searchboxMap">
-            <button type="submit">Search</button>
-        </form>
-    </div>
+<div class="searchDiv">
+    <form action="" method="get">
+        <input type="search" name="searchboxMap" id="searchboxMap">
+        <button type="submit">Search</button>
+    </form>
+</div>
 
+<div id="osm-map" style="height:300px, width:600px"></div>
 
-
-    <div id="osm-map"></div>
-
-    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
 <script src="/js/generalMap.js"></script>
-</body>
-</html>
