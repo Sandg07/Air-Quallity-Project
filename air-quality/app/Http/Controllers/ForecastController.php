0b16bebf46e1129cd82890420f7b24267a6c6c2c
Paul Lallemand <paul.lallemand92@gmail.com>
2021-12-14 16:12:19 +0100

    switch statement

--- air-quality/app/Http/Controllers/ForecastController.php
+++ air-quality/app/Http/Controllers/ForecastController.php
@@ -37,9 +37,79 @@ class ForecastController extends Controller
 
     public function calculatingDayAverage($day = '01')
     {
+        $stationDayly = [
+            'Esch TES11' => [
+                'pm10' => 0,
+                'no2' => 0,
+                'o3' => 0,
+                'x' => '49.5050417',
+                'y' => '5.976887',
+                'i' => 0,
+            ],
+            'Esch TEG12' => [
+                'pm10' => 0,
+                'no2' => 0,
+                'o3' => 0,
+                'x' => '49.494166',
+                'y' => '5.9847832',
+                'i' => 0,
+            ],
+            'Oberpallen TBK11' => [
+                'pm10' => 0,
+                'no2' => 0,
+                'o3' => 0,
+                'x' => '49.7318503',
+                'y' => '5.8471282',
+                'i' => 0,
+            ],
+            'Vianden TVI11' => [
+                'pm10' => 0,
+                'no2' => 0,
+                'o3' => 0,
+                'x' => '49.9439319',
+                'y' => '6.1759306',
+                'i' => 0,
+            ],
+            'Beidweiler TBW11' => [
+                'pm10' => 0,
+                'no2' => 0,
+                'o3' => 0,
+                'x' => '49.7222753',
+                'y' => '6.3052517',
+                'i' => 0,
+            ],
+            'Luxembourg TLB11' => [
+                'pm10' => 0,
+                'no2' => 0,
+                'o3' => 0,
+                'x' => '49.5977126',
+                'y' => '6.1375938°',
+                'i' => 0,
+            ],
+            'Luxembourg TLW11' => [
+                'pm10' => 0,
+                'no2' => 0,
+                'o3' => 0,
+                'x' => '49.6115433',
+                'y' => '6.1184772',
+                'i' => 0,
+            ],
+        ];
+
         foreach ($this->getMonthData() as $dataArray) {
             foreach ($dataArray as $dataDay) {
-                echo $dataDay->generated;
+                if (explode('/', strstr($dataDay->generated, ' ', true))[2] == $day) {
+                    foreach ($dataDay->station as $station) {
+                        switch ($station->code) {
+                            case 'TES11':
+                                $stationDayly[0]['o3'] += $station->data[0]->value;
+                                $stationDayly[0]['no2'] += $station->data[1]->value;
+                                $stationDayly[0]['pm10'] += $station->data[2]->value;
+                                $stationDayly[0]['i']++;
+                                break;
+                        }
+                    }
+                };
             }
         }
     }
