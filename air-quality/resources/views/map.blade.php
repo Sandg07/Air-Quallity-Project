6bdd1089f40871372c8bfcc2cc5f8d7a10cab090
Paul Lallemand <paul.lallemand92@gmail.com>
2021-12-15 16:41:01 +0100

    ajax call works

--- air-quality/resources/views/map.blade.php
+++ air-quality/resources/views/map.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" rel="stylesheet" />
     <title>MAP</title>
 </head>
@@ -43,54 +44,18 @@
                 $.ajax({
                     url: "/map",
                     type: "POST",
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
                     data: {
                         value: value
-                    }
+                    },
 
-                    success: function(array) {
-                        console.log(array);
-
-                        function addPoint(LatLgn, color) {
-                            var circle = L.circle(LatLgn, {
-                                color: "transparent",
-                                fillColor: color,
-                                fillOpacity: 0.9,
-                                radius: 500,
-                            }).addTo(map);
-                        }
-                        var allpm10 = array.pollutant.pm10.forEach(function(data) {
-                            if (data.index == 1) {
-                                var color = "#4169E1";
-                            } else if (data.index == 2) {
-                                var color = "#7a96ea";
-                            } else if (data.index == 3) {
-                                var color = "#b3c3f3";
-                            } else if (data.index == 4) {
-                                var color = "#d9e1f9";
-                            } else if (data.index == 5) {
-                                var color = "#FFFF66";
-                            } else if (data.index == 6) {
-                                var color = "#FFCC00";
-                            } else if (data.index == 7) {
-                                var color = "#FF9800";
-                            } else if (data.index == 8) {
-                                var color = "#FF0000";
-                            } else if (data.index == 9) {
-                                var color = "#bf0000";
-                            } else if (data.index == 9) {
-                                var color = "#800000";
-                            }
-                            //this one use first y then x
-                            var LatLgn = L.latLng(data.y, data.x);
-                            addPoint(LatLgn, color);
-                        });
+                    success: function() {
+                        var pollutant = JSON.parse({!! json_encode($array['pollutant']) !!});
+                        console.log(pollutant);
 
 
-
-
-                        if (response) {
-                            $("#ajaxform")[0].reset();
-                        }
                     },
                     error: function(error) {
                         console.log(error);
