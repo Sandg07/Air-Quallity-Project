f958e15f5519e26b676dd7c67bef6c590ab476d4
Sandrine G <sandrine.gardini@gmail.com>
2021-12-19 13:23:08 +0100

    removing test template dashboard page - restore the topnavbar

--- air-quality/resources/views/topNavbar.blade.php
+++ air-quality/resources/views/topNavbar.blade.php
@@ -1,14 +1,13 @@
-<nav class="navbar navbar-expand-md justify-content-md-between bg-info p-4">
-    {{-- logo section --}}
+{{-- <div class="top-navbar navbar navbar-expand-lg "> --}}
+
+<nav class="navbar navbar-expand-sm bg-info p-4">
     <span class="logo-nav navbar-brand mb-0 h1" style="color: white">Lëtz Breathe
 
     </span>
-
-    {{-- navbar button --}}
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" id="burgerMenu"
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" id="test"
                 class="bi bi-list text-primary" viewBox="0 0 16 16">
                 <path fill-rule="evenodd"
                     d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
@@ -33,6 +32,7 @@
         </ul>
 
 </nav>
+
 
 </div>
 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
