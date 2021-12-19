0a386b9e98e985e7eb504c2f8656895f96dee87c
Sandrine G <sandrine.gardini@gmail.com>
2021-12-19 23:18:42 +0100

    button feature change

--- air-quality/resources/views/features.blade.php
+++ air-quality/resources/views/features.blade.php
@@ -111,8 +111,8 @@
                         </p>
                         <button class="logBtn btn btn-outline-secondary btn-lg px-4" style="text-decoration: none"
                             target="_blank">
-                            <a href="/login" class="text-info icon-move-right">Log in here
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray"
+                            <a href="/login" class="text-dark icon-move-right">Log in here
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black"
                                     class="bi bi-arrow-right" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd"
                                         d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
