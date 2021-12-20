bd90d6467d6f78667a716eb591ab5303a7d69eda
Sandrine G <sandrine.gardini@gmail.com>
2021-12-20 23:22:54 +0100

    image header ok

--- air-quality/resources/views/template.blade.php
+++ air-quality/resources/views/template.blade.php
@@ -34,7 +34,7 @@
     {{-- wave design --}}
     <footer class="footer pb-5 pt-10 bg-light mt-n8 position-relative">
         <div class="position-relative w-100 z-index-1 top-0 mt-n3">
-            <svg width="100%" viewBox="0 -2 1920 157" version="1.1" xmlns="http://www.w3.org/2000/svg"
+            <svg width="100%" viewBox="0 1 1920 157" version="1.1" xmlns="http://www.w3.org/2000/svg"
                 xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>wave-down</title>
                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -59,16 +59,26 @@
                 </div>
                 <div class="col-lg-8 text-end ">
                     <div class="column flex-wrap text-center">
-                        <span> Catiana Meyer Miranda</span>
-                        <a href="https://www.linkedin.com/in/catiana-meyer-miranda/" class="text-secondary me-xl-4 me-4">
+
+                        <a href="https://www.linkedin.com/in/catiana-meyer-miranda/" class="text-secondary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor"
                                 class="bi bi-linkedin" viewBox="0 0 20 16">
                                 <path
                                     d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                             </svg>
                         </a>
-                        <span>Paul Lallemand</span>
-                        <a href="https://www.linkedin.com/in/paullallemand/" class="text-secondary me-xl-4 me-4">
+                        <span class="me-4">Catiana Meyer Miranda</span>
+
+                        <a href="https://www.linkedin.com/in/paullallemand/" class="text-secondary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-linkedin" viewBox="0 0 20 16">
+                                <path
+                                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+                            </svg>
+                        </a>
+                        <span class="me-4">Paul Lallemand</span>
+
+                        <a href="https://www.linkedin.com/in/sandrine-gardini-viger/" class="text-secondary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                                 class="bi bi-linkedin" viewBox="0 0 20 16">
                                 <path
@@ -76,14 +86,6 @@
                             </svg>
                         </a>
                         <span>Sandrine Gardini-Viger</span>
-                        <a href="https://www.linkedin.com/in/sandrine-gardini-viger/"
-                            class="text-secondary me-xl-4 me-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-linkedin" viewBox="0 0 20 16">
-                                <path
-                                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
-                            </svg>
-                        </a>
                     </div>
                     <hr class="horizontal dark col-lg-6 mt-5">
                 </div>
