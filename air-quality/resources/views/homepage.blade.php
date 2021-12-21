802abca5784ab9b8c2144778cc70bcf550458d61
Sandrine G <sandrine.gardini@gmail.com>
2021-12-21 10:06:26 +0100

    final header

--- air-quality/resources/views/homepage.blade.php
+++ air-quality/resources/views/homepage.blade.php
@@ -19,13 +19,13 @@
             <div class="d-grid d-md-flex justify-content-md-end position-absolute fixed-bottom me-4 me-lg-5 p-4">
                 <a class="btn btn-primary btn-lg px-4 me-md-2" href="{{ url('/login') }}">Login</a>
                 <a class="btn btn-outline-secondary btn-lg px-4" href="{{ url('/register') }}">Register</a>
-                {{-- <button type="button" class="btn btn-outline-secondary btn-lg px-4 ">Register</button> --}}
+
             </div>
         </div>
 
     </header>
 
-    {{-- <div style="margin:40px"></div> --}}
+
     {{-- MAIN CONTENT --}}
     <main class="container-lg flex-column justify-content-around mt-3">
 
@@ -95,21 +95,3 @@
 
 
 @endsection
-
-{{-- <header class="container col-xxl-8 px-4 py-5">
-        <div class="row flex-lg-row-reverse align-items-center gx-5 py-5">
-            <div class="col-lg-6">
-                <h1 class="hero-h1-title display-5 fw-bold lh-1 mb-3">Air Quality app</h1>
-                <p class="lead">I'm baby letterpress DIY leggings occupy copper mug, kogi affogato pabst vaporware
-                    selvage forage VHS fixie synth. </p>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Login</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Register</button>
-                </div>
-            </div>
-            <div class="col-10 col-sm-8 col-lg-6">
-                <img src="{{ URL::to('/assets/testheader.svg') }}" class="d-block mx-lg-auto img-fluid" width="700"
-                    height="500" loading="lazy" alt="...">
-            </div>
-        </div>
-    </header> --}}
